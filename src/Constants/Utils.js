19e05a3512ef0e7c7dd36106bd40a1a1fe31fc39
Vũ Nguyễn <huyvu0505@gmail.com>
2019-07-29 10:31:00 +0700

    Remove something

--- src/Constants/Utils.js
+++ src/Constants/Utils.js
@@ -9,32 +9,3 @@ export const setState = instance => newState => new Promise(resolve => instance.
 export const getValue = (path, defaultValue = undefined) => (object) => {
     return get(object, path, defaultValue);
 };
-
-
-// get number from object
-export const getNumber = (path, defaultValue = 0) => (object) => {
-    try {
-        return parseInt(get(object, path, defaultValue));
-    } catch (e) {
-        console.warn(`Can't get number from ${path}`);
-        return defaultValue;
-    }
-};
-
-//fetch api return promise => {success, error, ...payload}
-export const handleApi = async (func) => {
-    if (!func) return { success: false };
-    return func()
-        .then(data => {
-            return ({ success: true, ...data });
-        })
-        .catch(error => {
-            return ({ success: false, error });
-        });
-};
-
-
-//remove
-export const removeFalsey = (obj) => {
-    return pickBy(obj, identity);
-};
